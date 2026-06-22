a28bc7af0f6810616cd7331fd5ffcef2a841099e
neondeathstar1 <besundbn@mail.uc.edu>
2026-06-22 14:20:23 -0400

    Init

---
+++ src/Lab_04_02_YearlyMaintenanceCost.java
@@ -0,0 +1,16 @@
+public class Lab_04_02_YearlyMaintenanceCost  {
+    public static void main(String[] args) {
+
+        double springMaintenanceCost = 450.00;
+        double summerMaintenanceCost = 320.00;
+        double fallMaintenanceCost = 275.00;
+        double winterMaintenanceCost = 600.00;
+
+        double totalYearlyMaintenanceCost = springMaintenanceCost + summerMaintenanceCost + fallMaintenanceCost + winterMaintenanceCost;
+
+        System.out.println("Spring maintenance cost: $" + springMaintenanceCost);
+        System.out.println("Summer maintenance cost: $" + summerMaintenanceCost);
+        System.out.println("Fall maintenance cost: $" + fallMaintenanceCost);
+        System.out.println("Winter maintenance cost: $" + winterMaintenanceCost);
+        System.out.println("Total yearly maintenance cost: $" + totalYearlyMaintenanceCost);
+    }}
